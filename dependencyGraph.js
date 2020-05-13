e9a0b7bd67b60acc66f9680b931e98a2e5eb00d5
YashIsDeep <yashparth.gupta@gmail.com>
2020-05-13 11:43:47 +0530

    Added node and jest modules

---
+++ dependencyGraph.js
@@ -0,0 +1,21 @@
+// Directed dependency graph
+class DependencyGraph
+{
+	constructor()
+	{
+		this.vertices={};
+		this.edges={};
+		this.labelMap={};
+		this.invLabelMap={};
+	}
+	addVertexByLabel(label)
+	{
+		
+	}
+	addEdgeByLabel(label_from,label_to)
+	{
+
+	}
+
+
+}
