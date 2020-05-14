ca2bac2de8f4814fdb4ebfb9e59562a896a194b8
YashIsDeep <yashparth.gupta@gmail.com>
2020-05-14 11:58:46 +0530

    Test commit

--- dependencyGraph.js
+++ dependencyGraph.js
@@ -34,7 +34,7 @@ module.exports= class DependencyGraph{
 		let vert=this.labelMap[label];
 		this.infected[vert]=true;
 	}
-	// hack hack hack hack
+	// hack hack
 	// Spreads the infection of an infected node
 	spread(root,hasSpread)
 	{
